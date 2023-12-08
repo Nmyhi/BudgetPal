95f88318a7794f9ad09f709b994e213099b746b1
Nmyhi <>
2023-12-08 22:37:06 +0000

    Modify BudgetPal position relative to the navbar with javascript

--- budgetpal/static/js/script.js
+++ budgetpal/static/js/script.js
@@ -3,3 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.querySelector("nav");
+  var logo = document.querySelector(".brand-logo img");
+
+  // Set the navbar height to the logo height
+  navbar.style.height = logo.offsetHeight + "px";
+});
