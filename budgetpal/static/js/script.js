bb12588a742c10f646445242f44dadbef59536f2
Nmyhi <>
2023-12-09 19:30:39 +0000

    Add template form to add_income.html

--- budgetpal/static/js/script.js
+++ budgetpal/static/js/script.js
@@ -5,9 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  var navbar = document.querySelector("nav");
-  var logo = document.querySelector(".brand-logo img");
+    var navbar = document.querySelector("nav");
+    var logo = document.querySelector(".brand-logo img");
 
-  // Set the navbar height to the logo height
-  navbar.style.height = logo.offsetHeight + "px";
+    // Set the navbar height to the logo height
+    navbar.style.height = logo.offsetHeight + "px";
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let datePicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datePicker);
 });
